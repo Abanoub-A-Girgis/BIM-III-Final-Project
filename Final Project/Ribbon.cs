f0bcdd9b7e21f60cc69e5c2e3fa5608532946517
Abanoub Girgis Aziz Girgis <bebo.hema.95@gmail.com>
2022-07-25 15:17:28 +0200

    Add project files.

---
+++ Final Project/Ribbon.cs
@@ -0,0 +1,74 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+
+namespace Ribbon
+{
+    internal class Ribbon : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+
+            application.CreateRibbonTab("Scheduling to CSV");
+
+            RibbonPanel ribbonpanel =  application.CreateRibbonPanel("Scheduling to CSV", "Scheduling in Revit");
+            RibbonPanel ribbonpanel2 = application.CreateRibbonPanel("Scheduling to CSV", "Scheduling to CSV");
+
+            string path = Assembly.GetExecutingAssembly().Location;
+
+            PushButtonData button1 = new PushButtonData("Button 1", "Create Beam Schedule", path, "Final_Project.Beams");
+            PushButtonData button2 = new PushButtonData("Button 2", "Create Roof Schedule", path, "Final_Project.Roof");
+            PushButtonData button3 = new PushButtonData("Button 3", "Create Columns Schedule", path, "Final_Project.Columns");
+            PushButtonData button4 = new PushButtonData("Button 4", "Create Walls Schedule", path, "Final_Project.Walls");
+            PushButtonData button5 = new PushButtonData("Button 5", "Create Doors Schedule", path, "Final_Project.Doors");
+            PushButtonData button6 = new PushButtonData("Button 6", "Create Windows Schedule", path, "Final_Project.Windows");
+            PushButtonData button7 = new PushButtonData("Button 7", "Create Floors Schedule", path, "Final_Project.Floors");
+
+            PushButtonData button8 = new PushButtonData("Button 7", "Export Schedules", path, "Final_Project.ExportAllSchedules");
+
+            PushButton pushButton1 = ribbonpanel.AddItem(button1) as PushButton;
+            PushButton pushButton2 = ribbonpanel.AddItem(button2) as PushButton;
+            PushButton pushButton3 = ribbonpanel.AddItem(button3) as PushButton;
+            PushButton pushButton4 = ribbonpanel.AddItem(button4) as PushButton;
+            PushButton pushButton5 = ribbonpanel.AddItem(button5) as PushButton;
+            PushButton pushButton6 = ribbonpanel.AddItem(button6) as PushButton;
+            PushButton pushButton7 = ribbonpanel.AddItem(button7) as PushButton;
+
+            PushButton pushButton8 = ribbonpanel2.AddItem(button8) as PushButton;
+
+            pushButton1.LargeImage = new BitmapImage(new Uri("pack://application:,,,/Final Project;component/Icons/Beam.png"));
+            pushButton2.LargeImage = new BitmapImage(new Uri("pack://application:,,,/Final Project;component/Icons/Roof.png"));
+            pushButton3.LargeImage = new BitmapImage(new Uri("pack://application:,,,/Final Project;component/Icons/Column.png"));
+            pushButton4.LargeImage = new BitmapImage(new Uri("pack://application:,,,/Final Project;component/Icons/Wall.png"));
+            pushButton5.LargeImage = new BitmapImage(new Uri("pack://application:,,,/Final Project;component/Icons/Door.png"));
+            pushButton6.LargeImage = new BitmapImage(new Uri("pack://application:,,,/Final Project;component/Icons/Window.png"));
+            pushButton7.LargeImage = new BitmapImage(new Uri("pack://application:,,,/Final Project;component/Icons/Floor.png"));
+
+            pushButton8.LargeImage = new BitmapImage(new Uri("pack://application:,,,/Final Project;component/Icons/Export.png"));
+
+            pushButton1.ToolTip = "Create Beams Scheduling";
+            pushButton2.ToolTip = "Create Roof Scheduling";
+            pushButton3.ToolTip = "Create Column Scheduling";
+            pushButton4.ToolTip = "Create Wall Scheduling";
+            pushButton5.ToolTip = "Create Door Scheduling";
+            pushButton6.ToolTip = "Create Window Scheduling";
+            pushButton7.ToolTip = "Create Floor Scheduling";
+
+            pushButton8.ToolTip = "Export All Created Schedules to CSV";
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Final Project/UI.xaml.cs
@@ -0,0 +1,134 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Export_UI
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UI : Window
+    {
+        public UI()
+        {
+            InitializeComponent();
+        }
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            Close();
+        }
+        string path;
+        private void SelectFolder(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+                path = dialog.SelectedPath;
+            }
+        }
+        bool beams, roofs, columns, walls, doors, windows, floors;
+        private void Beams_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((sender as CheckBox).IsChecked == true)
+            {
+                beams = true;
+            }
+            else
+            {
+                beams = false;
+            }
+        }
+
+        private void Roofs_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((sender as CheckBox).IsChecked == true)
+            {
+                roofs = true;
+            }
+            else
+            {
+                roofs = false;
+            }
+        }
+
+        private void Columns_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((sender as CheckBox).IsChecked == true)
+            {
+                columns = true;
+            }
+            else
+            {
+                columns = false;
+            }
+        }
+
+        private void Walls_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((sender as CheckBox).IsChecked == true)
+            {
+                walls = true;
+            }
+            else
+            {
+                walls = false;
+            }
+        }
+
+        private void Doors_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((sender as CheckBox).IsChecked == true)
+            {
+                doors = true;
+            }
+            else
+            {
+                doors = false;
+            }
+        }
+
+        private void Windows_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((sender as CheckBox).IsChecked == true)
+            {
+                windows = true;
+            }
+            else
+            {
+                windows = false;
+            }
+        }
+
+        private void Floors_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((sender as CheckBox).IsChecked == true)
+            {
+                floors = true;
+            }
+            else
+            {
+                floors = false;
+            }
+        }
+
+        private void Export_Click(object sender, RoutedEventArgs e)
+        {
+            Final_Project.Helpers.setPath(path);
+            bool[] boolList = {beams, roofs, columns, walls, doors, windows, floors};
+            Final_Project.Helpers.setBool(boolList);
+            Close();
+        }
+    }
+}
